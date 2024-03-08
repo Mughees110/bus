2949c0531701c82956430200fb7309138fe70cd2
Mughees110 <zaidimughees@gmail.com>
2024-03-08 20:20:02 +0500

    ds

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/create-super-admin', function () {
+    $user=new User;
+    $user->name="super admin";
+    $user->email="superadmin@gmail.com";
+    $user->password=Hash::make('123456');
+    $user->role="super-admin";
+    $user->save();
+    dd('donw');
 });
 Route::get('clear',function(){
 	Artisan::call('config:cache');
@@ -22,3 +32,4 @@ Route::get('clear',function(){
      Artisan::call('view:clear');
      return 'Routes cache cleared';
 });
+
